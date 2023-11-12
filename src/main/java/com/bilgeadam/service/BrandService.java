3fca8b19010152b007fea6086ed578122f3a2a00
alperenikinci <alperen.ikinci@hotmail.com>
2023-11-12 15:49:30 +0300

    Programın altyapısı kuruldu...
    Signed-off-by: alperenikinci <alperen.ikinci@hotmail.com>

---
+++ src/main/java/com/bilgeadam/service/BrandService.java
@@ -0,0 +1,28 @@
+package com.bilgeadam.service;
+
+import com.bilgeadam.dto.request.BrandSaveRequestDto;
+import com.bilgeadam.mapper.BrandMapper;
+import com.bilgeadam.repository.BrandRepository;
+import com.bilgeadam.repository.entity.Brand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BrandService {
+
+    private final BrandRepository brandRepository;
+
+
+
+    public Brand saveBrand(BrandSaveRequestDto dto){
+        //TODO Brand daha önce kaydedilmiş mi? Bunu kontrol etmek için bir metot yazılacak.
+        return brandRepository.save(BrandMapper.INSTANCE.fromSaveRequestToBrand(dto));
+    }
+
+    public Optional<Brand> findByBrandName(String name){
+        return brandRepository.findByBrandName(name);
+    }
+}
